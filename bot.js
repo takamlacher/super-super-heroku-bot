31ea043639f2f6c5fe76e4d472280c6c2277afff
takamlacher <github@bottein.com>
2018-02-19 21:07:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const fetch = require('node-fetch');
 
 bot.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
